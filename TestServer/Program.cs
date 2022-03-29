17750f17524547622ec337a7997ed9c776e77a7a
DjNemas <922996+DjNemas@users.noreply.github.com>
2022-03-29 06:16:55 +0200

    Add files via upload

---
+++ TestServer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Server.StartServer();
+            while (true)
+            {
+                Console.WriteLine("What do you want to send?");
+                string str = Console.ReadLine();
+                if (Clients.clientsList[0] != null)
+                {
+                    Clients.clientsList[0].SendData(Encoding.UTF8.GetBytes(str));
+                }
+            }
+        }
+    }
+}
---
+++ TestServer/Server.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestServer
+{
+    internal class Server
+    {
+        private static TcpListener tcpListener;
+
+        public static void StartServer()
+        {
+            Console.WriteLine("Starting Server...");
+
+            tcpListener = new TcpListener(IPAddress.Any, 3939);
+            tcpListener.Start();
+            Console.WriteLine("Waiting for new Clients...");
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+        }
+
+        private static void TCPConnectCallback(IAsyncResult result)
+        {
+            TcpClient tcpClient = tcpListener.EndAcceptTcpClient(result);
+            Console.WriteLine("Accepted new Client");
+            Clients client = new Clients(tcpClient);
+
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+        }
+
+    }
+}
